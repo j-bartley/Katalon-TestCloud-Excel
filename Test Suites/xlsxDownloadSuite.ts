7ccabc747096df709385fc25a624f9ea54b552fb
j-bartley <jordan.bartley@hotmail.com>
2024-07-17 14:30:19 -0500

    Added Test Suite

---
+++ Test Suites/xlsxDownloadSuite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>xlsxDownloadSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f109431a-6c30-4f6a-8fc0-5d196cd53c18</testSuiteGuid>
+   <testCaseLink>
+      <guid>aef4bcc5-1602-41b7-bc79-5db44a2bb60d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/xlsxDownloadTest</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
